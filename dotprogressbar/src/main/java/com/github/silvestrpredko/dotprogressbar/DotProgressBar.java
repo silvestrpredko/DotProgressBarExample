7b70c2525a53ea68f4f0a47e92621aa85fb0f6c5
silvestr <predko.silvestr@gmail.com>
2015-08-28 19:54:47 +0300

    it uploadsgit status!

--- dotprogressbar/src/main/java/com/github/silvestrpredko/dotprogressbar/DotProgressBar.java
+++ dotprogressbar/src/main/java/com/github/silvestrpredko/dotprogressbar/DotProgressBar.java
@@ -16,7 +16,7 @@ import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.Transformation;
 
-import com.silvestr.dotprogressbar.R;
+import com.github.silvestrpredko.dotprogressbar.R;
 
 /**
  * @author Silvestr Predko.
@@ -59,7 +59,7 @@ public class DotProgressBar extends View {
 
   /**
    * Colors
-   * */
+   */
   private int startColor;
   private int endColor;
 
@@ -225,16 +225,16 @@ public class DotProgressBar extends View {
     startAnimation(bounceAnimation);
   }
 
-  public static int darker (int color, float factor) {
-    int a = Color.alpha( color );
+  public static int darker(int color, float factor) {
+    int a = Color.alpha(color);
     int r = Color.red(color);
-    int g = Color.green( color );
-    int b = Color.blue( color );
+    int g = Color.green(color);
+    int b = Color.blue(color);
 
-    return Color.argb( a,
-        Math.max( (int)(r * factor), 0 ),
-        Math.max( (int)(g * factor), 0 ),
-        Math.max( (int)(b * factor), 0 ) );
+    return Color.argb(a,
+        Math.max((int) (r * factor), 0),
+        Math.max((int) (g * factor), 0),
+        Math.max((int) (b * factor), 0));
   }
 
   @Override
